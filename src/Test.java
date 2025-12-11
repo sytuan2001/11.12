8cda629d06b6acb705975ca9c2b0a4b2b295b5da
sytuan2001 <thuancat1978@gmail.com>
2025-12-11 16:54:01 +0700

    add

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    // Main method
+    public static void main(String[] args) {
+/** Display output */
+        System.out.println("Welcome to Java");
+        System.out.println("hello");
+    }
+}
